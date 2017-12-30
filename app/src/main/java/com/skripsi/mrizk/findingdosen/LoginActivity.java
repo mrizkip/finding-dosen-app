8ff42f69546cc5f75a039226aac3879922e31157
QIDONK\mrizk_000 <mrizkip@hotmail.com>
2017-12-30 21:19:17 +0700

    register and login view

---
+++ app/src/main/java/com/skripsi/mrizk/findingdosen/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.skripsi.mrizk.findingdosen;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.iconics.Iconics;
+import com.mikepenz.iconics.context.IconicsContextWrapper;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
