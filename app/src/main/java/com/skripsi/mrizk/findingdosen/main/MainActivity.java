29f9ce9c712e63e558b01a17a9d9d35baaaf4861
Miftahul Rizki Purwonegoro <mrizkip@student.ub.ac.id>
2018-03-04 08:51:48 +0700

    bind view

--- app/src/main/java/com/skripsi/mrizk/findingdosen/main/MainActivity.java
+++ app/src/main/java/com/skripsi/mrizk/findingdosen/main/MainActivity.java
@@ -9,8 +9,12 @@ import android.content.pm.PackageManager;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Build;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.Button;
 
@@ -23,9 +27,20 @@ import com.skripsi.mrizk.findingdosen.R;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+
+    @BindView(R.id.mainActivity_toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.mainActivity_searchView)
+    SearchView searchView;
+    @BindView(R.id.mainActivity_recyclerView)
+    RecyclerView recyclerView;
+    ActionBar actionBar;
 
     private WifiManager wifiManager;
     private List<ScanResult> scanResults;
@@ -35,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         Dexter.withActivity(this)
                 .withPermissions(
